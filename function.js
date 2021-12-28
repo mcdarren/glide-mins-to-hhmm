04c1147a45e6359ef98226fee4e1acab011d0677
Darren Murphy <darren@victoriajd.com>
2021-12-29 00:30:54 +0800

    Rename parameter

--- function.js
+++ function.js
@@ -1,6 +1,6 @@
-window.function = function (total_mins) {
-  if (total_mins.value === undefined) return undefined;
-  var hours = Math.floor(total_mins.value/60);
-  var mins = total_mins.value % 60;
+window.function = function (total) {
+  if (total.value === undefined) return undefined;
+  var hours = Math.floor(total.value/60);
+  var mins = total.value % 60;
   return hours.toString().padStart(2, '0') + ":" + mins.toString().padStart(2, '0');
 }
