8cc2a42559cf782ff5f0d5cd4f6afd7e678e5a56
Darren Murphy <darren@victoriajd.com>
2021-12-29 00:37:33 +0800

    Return undef for zero minutes

--- function.js
+++ function.js
@@ -1,5 +1,6 @@
 window.function = function (total) {
   if (total.value === undefined) return undefined;
+  if (total.value === 0) return undefined;
   var hours = Math.floor(total.value/60);
   var mins = total.value % 60;
   return hours.toString().padStart(2, '0') + ":" + mins.toString().padStart(2, '0');
